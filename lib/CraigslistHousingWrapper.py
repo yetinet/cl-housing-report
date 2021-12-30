1707b63a8791e1a0d4d9980a77951c7235aa5a02
Daniel Nelson <danielnelson@Daniels-MacBook-Pro.local>
2021-12-29 19:00:25 -0800

    XL.  addressed broken saveToFile and getPostsFromFile, added 2 metrics, refactored saveToFile and getPostsFromFile from Posts to Dataprocessing

--- lib/CraigslistHousingWrapper.py
+++ lib/CraigslistHousingWrapper.py
@@ -4,5 +4,5 @@ from craigslist import CraigslistHousing
 # WrapperClass
 class CraigslistHousingWrapper(CraigslistHousing):
 
-    def __init__(self, site="sfbay", area="scz", category="hhh", filters={"max_bedrooms":1000, "max_bathrooms":1000, "max_ft2":99999}, log_level=WARN):
+    def __init__(self, site="sfbay", area="scz", category="apa", filters={"max_bedrooms":1000, "max_bathrooms":1000, "max_ft2":99999}, log_level=WARN):
         super().__init__(site=site, area=area, category=category, filters=filters, log_level=log_level)
